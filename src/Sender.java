9c6382480860e71ace65e70588738c38ce510a9c
loewey <grandmasterlouis@gmail.com@>
2025-02-19 11:44:09 +0000

    implemented ClientIP to check for receiver IP and port number

--- src/Sender.java
+++ src/Sender.java
@@ -6,9 +6,23 @@ import CMPC3M06.AudioRecorder;
 
 public class Sender
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
+        int port = 5555;
 
+        InetAddress clientIP = null;
+        AudioRecorder recorder = new AudioRecorder();
+
+        try
+        {
+            //temp localhost
+            clientIP =InetAddress.getByName("localhost");
+        } catch(UnknownHostException e)
+        {
+            System.err.println("ERROR : TextSender : could not find client IP ");
+            e.printStackTrace();
+            System.exit(0);
+        }
     }
 
 }
