758ebb85481ce6841a4b90458fc984f0e7ba1283
loewey <grandmasterlouis@gmail.com@>
2025-02-20 12:54:02 +0000

    UNTESTED changes to both Sender and Receiver class

--- src/sendrecieve/Receiver.java
+++ src/sendrecieve/Receiver.java
@@ -2,7 +2,9 @@ package sendrecieve;
 
 import java.net.*;
 import java.io.*;
+import java.util.Arrays;
 
+import CMPC3M06.AudioPlayer;
 
 public class Receiver implements Runnable {
 
@@ -15,37 +17,72 @@ public class Receiver implements Runnable {
 
     public void run() {
         int port = 55555;
+        boolean running = true;
+        AudioPlayer player = null;
 
         try {
+            player = new AudioPlayer();
+        }catch(Exception e)
+        {
+            return;
+        }
+        try {
             receivingSocket = new DatagramSocket(port);
+            receivingSocket.setSoTimeout(500);
         } catch (SocketException e) {
             System.out.println("ERROR Receiver: Could not open UDP packet to send from");
             e.printStackTrace();
             System.exit(0);
         }
 
-        for (int i = 0; i < 999; i++) {
+        byte[] buffer =new byte[512];
+
+        while(running){
+            DatagramPacket packet = new DatagramPacket(buffer, 0, buffer.length);
+
             try {
-                byte[] buffer = new byte[200];
-                DatagramPacket packet = new DatagramPacket(buffer, 0, 80);
-
-                receivingSocket.setSoTimeout(30);
-
-                try {
-                    receivingSocket.receive(packet);
-                    String str = new String(buffer);
-                    System.out.println(str);
-                } catch (SocketTimeoutException e) {
-                    System.out.println(".");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } catch (IOException e) {
-                System.out.println("ERROR Receiver: Some IO error occured");
+                receivingSocket.receive(packet);
+            }catch(SocketTimeoutException e){
+                System.out.println(".");
+            }catch(Exception e) {
+                System.out.println("ERROR : something else");
                 e.printStackTrace();
+            }
+            try{
+                if (packet.getLength() > 0){
+                    byte[] audioBlock = Arrays.copyOf(packet.getData(),packet.getLength());
 
+                    player.playBlock(audioBlock);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
             }
         }
+
+
+
+//        for (int i = 0; i < 999; i++) {
+//            try {
+//                byte[] buffer = new byte[200];
+//                DatagramPacket packet = new DatagramPacket(buffer, 0, 80);
+//
+//                receivingSocket.setSoTimeout(30);
+//
+//                try {
+//                    receivingSocket.receive(packet);
+//                    String str = new String(buffer);
+//                    System.out.println(str);
+//                } catch (SocketTimeoutException e) {
+//                    System.out.println(".");
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            } catch (IOException e) {
+//                System.out.println("ERROR Receiver: Some IO error occured");
+//                e.printStackTrace();
+//
+//            }
+//        }
         receivingSocket.close();
     }
 }
--- src/sendrecieve/Sender.java
+++ src/sendrecieve/Sender.java
@@ -1,9 +1,11 @@
 package sendrecieve;
 
+import CMPC3M06.AudioRecorder;
 import java.io.*;
 import java.net.*;
 
 public class Sender implements Runnable {
+
     static DatagramSocket sendingSocket;
 
     public void start() {
@@ -14,6 +16,18 @@ public class Sender implements Runnable {
     public void run() {
         int port = 55555;
         InetAddress clientIP = null;
+        AudioRecorder recorder = null;
+        int runtime = 10;
+
+        try {
+            recorder = new AudioRecorder();
+        } catch(Exception e){
+            System.err.println("ERROR : audioRecorder failed to initialize");
+            e.printStackTrace();
+            return;
+        }
+
+
         try {
             //temp localhost
             clientIP =InetAddress.getByName("localhost");
@@ -31,20 +45,34 @@ public class Sender implements Runnable {
             System.exit(0);
         }
 
-        for (int i = 0; i < 999; i++) {
-            try {
-                String str = "Packet " + i;
-
-                byte[] buffer = str.getBytes();
-
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, clientIP, port);
-
+        try {
+            for(int i=0;i<Math.ceil(runtime/0.032);i++){
+                byte[] block = recorder.getBlock();
+                DatagramPacket packet = new DatagramPacket(block, block.length, clientIP, port);
                 sendingSocket.send(packet);
-            }catch (IOException e) {
-                System.out.println("ERROR Sender 2: Some random IO error occured");
-                e.printStackTrace();
             }
+            recorder.close();
+        } catch(Exception e){
+            return;
         }
+
+
+
+
+       // for (int i = 0; i < 999; i++) {
+       //     try {
+       //         String str = "Packet " + i;
+
+       //         byte[] buffer = str.getBytes();
+
+       //         DatagramPacket packet = new DatagramPacket(buffer, buffer.length, clientIP, port);
+
+       //         sendingSocket.send(packet);
+       //     }catch (IOException e) {
+       //         System.out.println("ERROR Sender 2: Some random IO error occured");
+       //         e.printStackTrace();
+       //     }
+       // }
         sendingSocket.close();
     }
 }
