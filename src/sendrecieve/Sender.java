48c81eafe61ca4c6249c85988a1dc1046991f69d
Aiden Ayers <azurefalctis@proton.me>
2025-02-28 13:39:19 +0000

    Implemented Diffie-Hellman to generate symmetric key

--- src/sendrecieve/Sender.java
+++ src/sendrecieve/Sender.java
@@ -1,16 +1,21 @@
 package sendrecieve;
 
 import java.io.*;
+import java.math.BigInteger;
 import java.net.*;
 import java.nio.ByteBuffer;
-
+import java.util.Random;
 import CMPC3M06.AudioRecorder;
-
-import javax.xml.crypto.Data;
 
 public class Sender implements Runnable {
     static DatagramSocket sendingSocket;
+    static DatagramSocket sendingKeySocket;
+    static DatagramSocket receivingKeySocket;
     private AudioRecorder recorder;
+    private final long p = 66371551L;
+    private final long g = 6L;
+    private final Random rand = new Random(System.currentTimeMillis());
+    private final long x = rand.nextLong();
 
     public void audioRecorder()throws Exception{
         recorder = new AudioRecorder();
@@ -21,9 +26,43 @@ public class Sender implements Runnable {
         thread.start();
     }
 
+    // Key exchange method using TCP sockets. Sender acts as the server
+    public BigInteger keyExchange() {
+        long key = 0;
+        BigInteger K = BigInteger.valueOf(0);
+        try {
+
+            // Opens a Socket on port 5000
+            Socket socket = new Socket("localhost", 5000);
+
+            // Output stream becomes client input stream
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            // Input stream receives data from client output stream
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+
+            BigInteger R = BigInteger.valueOf(g).modPow(BigInteger.valueOf(x), BigInteger.valueOf(p));
+
+            // Sends the value of R to the client
+            output.writeLong(R.longValue());
+
+            // Reads the value Y sent from the client
+            BigInteger Y = BigInteger.valueOf(input.readLong());
+
+            K = Y.modPow(BigInteger.valueOf(x), BigInteger.valueOf(p));
+
+            return K;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return K;
+    }
+
+
     public void run() {
-        int port = 55555;
         InetAddress clientIP = null;
+        int port = 55555;
         try {
             //temp localhost
             clientIP =InetAddress.getByName("localhost");
@@ -41,33 +80,34 @@ public class Sender implements Runnable {
             System.exit(0);
         }
 
+        BigInteger symKey = keyExchange();
+
         int runTime = 10;
 
-        for (int i=0;i<Math.ceil(runTime/0.032);i++){
-            try{
-                byte[] audioBlock = recorder.getBlock();
+        while (true) {
+            for (int i = 0; i < Math.ceil(runTime / 0.032); i++) {
+                try {
+                    byte[] audioBlock = recorder.getBlock();
 
-                // Allocates a 514 byte long byte buffer
-                ByteBuffer buffer = ByteBuffer.allocate(514);
-                if(audioBlock != null){
+                    // Allocates a 514 byte long byte buffer
+                    ByteBuffer buffer = ByteBuffer.allocate(514);
+                    if (audioBlock != null) {
 
-                    // First 2 bytes of the packet will be a short representing the sequence number
-                    buffer.putShort((short) i);
+                        // First 2 bytes of the packet will be a short representing the sequence number
+                        buffer.putShort((short) i);
 
-                    // Remaining bits will be the audio block
-                    buffer.put(audioBlock);
-                    DatagramPacket packet = new DatagramPacket(buffer.array(), buffer.capacity(), clientIP,port);
-                    sendingSocket.send(packet);
-                    System.out.println("audio packet sent, size :" + buffer.capacity() + " bytes");
+                        // Remaining bits will be the audio block
+                        buffer.put(audioBlock);
+                        DatagramPacket packet = new DatagramPacket(buffer.array(), buffer.capacity(), clientIP, port);
+                        sendingSocket.send(packet);
+                        System.out.println("audio packet sent, size :" + buffer.capacity() + " bytes");
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error : TextSender: Some random IO error has occurred");
+                    e.printStackTrace();
                 }
-            } catch(IOException e){
-                System.out.println("Error : TextSender: Some random IO error has occurred");
-                e.printStackTrace();
             }
         }
-        recorder.close();
-        sendingSocket.close();
-        System.out.println("Sending Socket is now closed");
        // for (int i = 0; i < 999; i++) {
        //     try {
        //         String str = "Packet " + i;
