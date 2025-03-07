16fd87c63cad9dcc80d2ba8ba18c682cb564fce0
Aiden <139812588+FulgurStrike@users.noreply.github.com>
2025-03-07 14:27:21 +0000

    Update Receiver.java

--- src/sendrecieve/Receiver.java
+++ src/sendrecieve/Receiver.java
@@ -150,28 +150,5 @@ public class Receiver implements Runnable {
             }
         }
         receivingSocket.close();
-//        for (int i = 0; i < 999; i++) {
-//            try {
-//                byte[] buffer = new byte[200];
-//                DatagramPacket packet = new DatagramPacket(buffer, 0, 80);
-//
-//                receivingSocket.setSoTimeout(30);
-//
-//                try {
-//                    receivingSocket.receive(packet);
-//                    String str = new String(buffer);
-//                    System.out.println(str);
-//                } catch (SocketTimeoutException e) {
-//                    System.out.println(".");
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            } catch (IOException e) {
-//                System.out.println("ERROR Receiver: Some IO error occured");
-//                e.printStackTrace();
-//
-//            }
-//        }
-//        receivingSocket.close();
     }
 }
